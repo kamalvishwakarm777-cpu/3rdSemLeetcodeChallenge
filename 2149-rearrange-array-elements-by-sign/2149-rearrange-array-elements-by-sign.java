56a7c8f0d79c45da1d8c867d4acd8b445ddd99d0
Archyansh_Adrish <kamalvishwakarm777@gmail.com>
2026-09-13 11:02:26 +0530

    Time: 4 ms (40.53%), Space: 176.6 MB (5.49%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -2,34 +2,28 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
 
-        int[] array1 = new int[n / 2];
-        int[] array2 = new int[n / 2];
+        int[] pos = new int[n / 2];
+        int[] neg = new int[n / 2];
 
         int p = 0;
         int q = 0;
+
         for(int i = 0;i < n;i++){
 
             if(nums[i] > 0){
-                array1[p++] = nums[i];
+                pos[p++] = nums[i];
             }
             else{
-                array2[q++] = nums[i];
+                neg[q++] = nums[i];
             }
         }
 
 
         int[] mergedArray = new int[n];
-        int i = 0;
-        int j = 0;
 
-        for(int k = 0;k < n;k++){
-
-            if(k % 2 == 0){
-                mergedArray[k] = array1[i++];
-            }
-            else {
-                mergedArray[k] = array2[j++];
-            }
+        for(int i = 0;i < n/2;i++){
+            mergedArray[2*i] = pos[i];
+            mergedArray[2*i + 1] = neg[i];
         }
 
         return mergedArray;
